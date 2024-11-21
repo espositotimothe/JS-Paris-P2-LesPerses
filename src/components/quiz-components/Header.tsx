4ba2f9fda99606dd46c8267d30d04f940a00fa4d
MARINA MASSOT <marina.massot1@gmail.com>
2024-11-21 10:48:45 +0100

    add h1 into component header

--- src/components/quiz-components/Header.tsx
+++ src/components/quiz-components/Header.tsx
@@ -11,27 +11,33 @@ export default function Header() {
 	return (
 		<>
 			{!showFilters ? (
-				<section className="welcome">
-					<h1 className="quiz-title">Quiz</h1>
-					<p className="quiz-description">
-						Découvrez notre sélection de quiz, incluant des classiques pour
-						parfaire vos connaissances, des quiz aléatoires pour les curieux en
-						quête de surprises, et un défi spécial inspiré du célèbre jeu
-						télévisé, le "Qui veut gagner des millions". Seul ou entre amis,
-						plongez dans l’aventure, apprenez, et relevez les défis pour tenter
-						d'atteindre le score parfait.
-					</p>
-					<p className="quiz-description">
-						Prêt à tester vos limites ? Que le jeu commence !
-					</p>
-					<button
-						type="button"
-						className="button-start"
-						onClick={handleStartQuiz}
-					>
-						Choisis ton quiz
-					</button>
-				</section>
+				<>
+					<section className="welcome">
+						<h1 className="welcome-title"> QUI VEUT DEBUGGER DES MILLIONS</h1>
+					</section>
+
+					<section className="quiz-block">
+						<h2 className="quiz-title">QUIZ</h2>
+						<p className="quiz-description">
+							Découvrez notre sélection de quiz, incluant des classiques pour
+							parfaire vos connaissances, des quiz aléatoires pour les curieux
+							en quête de surprises, et un défi spécial inspiré du célèbre jeu
+							télévisé, le "Qui veut gagner des millions". Seul ou entre amis,
+							plongez dans l’aventure, apprenez, et relevez les défis pour
+							tenter d'atteindre le score parfait.
+						</p>
+						<p className="quiz-description">
+							Prêt à tester vos limites ? Que le jeu commence !
+						</p>
+						<button
+							type="button"
+							className="button-start"
+							onClick={handleStartQuiz}
+						>
+							Choisis ton quiz
+						</button>
+					</section>
+				</>
 			) : (
 				<Themedif />
 			)}

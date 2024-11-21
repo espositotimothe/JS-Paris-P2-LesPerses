f46e59da44ef4d112bc00bae67f15ff38dda7899
Karim Ben_Harzallah <karim.benharzallah@hotmail.fr>
2024-11-21 11:04:48 +0100

    Final NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import type { Question } from "./components/Themedif";
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar.tsx";
 import Themedif from "./components/Themedif";
 
 function App() {
 	const [data, setData] = useState<Question[] | null>(null);
 	return (
 		<div>
+			<NavBar />
+
 			<Themedif data={data} setData={setData} />
 		</div>
 	);

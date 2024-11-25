66f94966e11d5c9a59769484285a2fd90b8d29a8
Karim <karim.benharzallah@hotmail.fr>
2024-11-25 10:15:37 +0100

    Update NavBar.tsx
    Suppressions des commentaires

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,27 +5,27 @@ const NavBar = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
 	const toggleMenu = () => {
-		setIsOpen(!isOpen); // Bascule l'état du menu
+		setIsOpen(!isOpen); 
 	};
 
 	return (
 		<nav className="navbar">
 			<img src="src/images/Quizz-nuage-violet-sans-fond.png" alt="Logo" />
 
-			{/* Menu burger, uniquement visible si isOpen est true */}
+			{}
 			<div
 				className="burger"
-				onClick={toggleMenu} // Clic pour ouvrir/fermer le menu
+				onClick={toggleMenu}
 				onKeyDown={(e) =>
 					e.key === "Enter" || e.key === " " ? toggleMenu() : null
-				} // Clavier (Enter/Espace)
+				}
 			>
 				<div className="bar" />
 				<div className="bar" />
 				<div className="bar" />
 			</div>
 
-			{/* Menu qui s'affiche lorsque isOpen est true */}
+			{}
 			<div className={`menu ${isOpen ? "open" : ""}`}>
 				<a href="#accueil">Accueil</a>
 				<a href="#million">Million</a>
